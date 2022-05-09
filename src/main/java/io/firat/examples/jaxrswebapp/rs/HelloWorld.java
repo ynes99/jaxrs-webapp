493d4d772f6434aeb794372fa82a3cd5d6be4f36
Ynes <ines.bouhelal@ensi-uma.tn>
2022-05-09 14:32:23 +0100

    initial commit

--- src/main/java/io/firat/examples/jaxrswebapp/rs/HelloWorld.java
+++ src/main/java/io/firat/examples/jaxrswebapp/rs/HelloWorld.java
@@ -1,60 +1,37 @@
 
 package io.firat.examples.jaxrswebapp.rs;
 
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
-
+import javax.ws.rs.core.Response;
 
 
 @Path("/")
 public final class HelloWorld {
-
-
-
-    //~ --- [CONSTRUCTORS] ---------------------------------------------------------------------------------------------
-
-    public HelloWorld() {
-
-    }
-
-
-
-    //~ --- [METHODS] --------------------------------------------------------------------------------------------------
-
-    @GET
-    @Path("/multiply/{a}/{b}")
-    @Produces(MediaType.TEXT_PLAIN)
-    public int mulitiply(@PathParam("a") final int a,
-            @PathParam("b") final int b) {
-
-        return a * b;
-    }
-
-
-
-    //~ ----------------------------------------------------------------------------------------------------------------
-
+    public HelloWorld() {}
     @GET
     @Path("/hello/{name}")
     @Produces(MediaType.TEXT_PLAIN)
     public String sayHello(@PathParam("name") final String name) {
-
-        return "Hello " + name + "!";
+        return "Hello " + name + "!!";
     }
-
-
-
-    //~ ----------------------------------------------------------------------------------------------------------------
-
     @GET
     @Path("/hello")
     @Produces(MediaType.TEXT_PLAIN)
     public String sayHelloWorld() {
-
         return "Hello World!";
+    }
+    @GET
+    @Path("/query")
+    public Response getUsers(
+            @QueryParam("hi") String hi,
+            @QueryParam("lan") String lan){
+        return Response
+                .status(200)
+                .entity("language : "+lan+" ==> "+hi).build();
     }
 }
